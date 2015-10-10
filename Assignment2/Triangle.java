d43199794519cc3bedf2570c61ae9f9981d79e79
ezhang16 <ezhang16@dashonline.org>
2015-10-10 00:32:30 -0500

    Create Triangle.java

---
+++ Assignment2/Triangle.java
@@ -0,0 +1,22 @@
+public class Triangle extends Shape implements Printable {
+      //integer for base and height
+      private int base;
+      private int height;
+      
+      //triangle constructor
+      public Triangle(int base, int height){
+            this.base = base;
+            this.height = height;
+            setShapeName("Triangle");
+            setShapeType("Triangle");
+      }
+      
+      public void printDescription(){
+            System.out.println("Base is " + base + "units.");
+            System.out.println("Height is " + height + "units.");
+      }
+      
+      public double getArea() {
+            return height*base*0.5
+      }
+}
